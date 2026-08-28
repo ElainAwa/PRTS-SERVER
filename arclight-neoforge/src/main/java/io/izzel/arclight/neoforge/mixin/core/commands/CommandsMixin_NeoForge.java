f7ea6d96e570e4696a5d8e27f380c5292a1f110f
Tianpao <nxtianpao@163.com>
2026-08-29 00:55:26 +0800

    fix: use asm Opcodes import for putfield injection point

--- arclight-neoforge/src/main/java/io/izzel/arclight/neoforge/mixin/core/commands/CommandsMixin_NeoForge.java
+++ arclight-neoforge/src/main/java/io/izzel/arclight/neoforge/mixin/core/commands/CommandsMixin_NeoForge.java
@@ -13,9 +13,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Opcodes;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
