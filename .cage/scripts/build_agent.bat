c7cefbc941d2488c9d4f8b654959cd107e8c67b0
ElainAwa <166903428+ElainAwa@users.noreply.github.com>
2026-08-31 14:14:01 +0800

    chore: untrack local cage workflow directory

--- .cage/scripts/build_agent.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-javac ClassDumpAgent.java
-jar cfm ClassDumpAgent.jar MANIFEST.MF ClassDumpAgent.class "ClassDumpAgent$1.class"
--- .cage/scripts/build_agent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-javac ClassDumpAgent.java
-jar cfm ClassDumpAgent.jar MANIFEST.MF ClassDumpAgent.class 'ClassDumpAgent$1.class'
